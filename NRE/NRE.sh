abc3396fce330fc29bd3bb1f1ec23c8e1a3fa54f
richard89 <richard89@debian.bcrowngroup.com>
2021-03-10 22:48:28 -0500

    adding macOS to bash script and drivers folder

--- NRE/NRE.sh
+++ NRE/NRE.sh
@@ -6,7 +6,7 @@ PROJECT_FOLDER=${PWD}
 #INSTALLING CHROMEDRIVER AND GECKODRIVER
 
 if [ $(uname) == "Darwin" ]; then
-DRIVER_FOLDER="${PROJECT_FOLDER}/drivers/linux/" ;
+DRIVER_FOLDER="${PROJECT_FOLDER}/drivers/macos/" ;
 elif [ $(uname) == "Linux" ]; then
 DRIVER_FOLDER="${PROJECT_FOLDER}/drivers/linux/" ;        
 else
@@ -16,6 +16,8 @@ fi &&
 sudo cp "${DRIVER_FOLDER}chromedriver" "$INSTALL_DIR"
 sudo cp "${DRIVER_FOLDER}geckodriver" "$INSTALL_DIR"
 
+sudo chmod +x "${INSTALL_DIR}/chromedriver"
+sudo chmod +x "${INSTALL_DIR}/geckodriver"
 ##################################################################
 
 
